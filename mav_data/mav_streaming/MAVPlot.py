59860deeaacc04cf9900e5afd82836719e8be781
xiaoke <wxk20000215@gmail.com>
2021-03-13 05:06:02 +0100

    Simplify

--- mav_data/mav_streaming/MAVPlot.py
+++ mav_data/mav_streaming/MAVPlot.py
@@ -43,13 +43,9 @@ def main():
     listener = Buffer(buffer_len = 512) # At sampling rate of 200Hz, 512 samples correspond to ~2.5 seconds of the most recent data.
     computer = BufferPlus(buffer_len = 15)
     # Setup multichannel plotter for visualisation:
-    plotter = MultichannelPlot(nchan = 8, xlen = 15) # Number of EMG channels in MYO armband is 8
-    #plotter1 = MAVMultichannelPlot(nchan = 16, xlen = 512)
+    plotter = MultichannelPlot(nchan = 8, xlen = 15) # Number of EMG channels in MYO armband is 8 , window size is 15 for MAV
   
 
-    mav_data = np.array([])
-    mav_data_final = np.zeros(1*8)
-    mav_data_final = mav_data_final[np.newaxis, :]  #1*8
     # Tell MYO API to start a parallel thread that will collect the data and
     # command the MYO to start sending EMG data.
     with hub.run_in_background(listener): # This is the way to associate our listener with the MYO API.
@@ -63,42 +59,9 @@ def main():
             if (emg_data.ndim==2):
                 mav_data = computer.get_mav_data(emg_data)
                 mav_data = np.array(mav_data)
-            #data.append(emg_data)
-            #data = np.array(data)
-
-            # if (emg_data.shape[0]>=512):
-                # mav_data = MAV(emg_data)             # 8*1
-                # mav_data = mav_data[np.newaxis, :]   #1*8
+           
                 
-            
-
-                #mav_data = np.array(mav_data)
-                #mav_data_final = np.array(mav_data_final)
-                # if (mav_data_final.shape[0]>=512):
-                    # mav_data_final = np.delete(mav_data_final,0,0)
-
-                # mav_data_final=np.r_[mav_data_final,mav_data]
-
-
-                #mav_data_final = mav_data_final.T
-                #data = np.delete(data,0, 0)
-
-
-            # Plot emg—_data
-            #plotter.update_plot(emg_data.T)
-            
-            
-            # Plot mav_data
-            #if (mav_data_final.T.ndim==2):
-                
-            plotter.update_plot(np.array(mav_data.T))
-                
-                
-            # Plot together 
-            #if (mav_data_final.T.ndim==2 & emg_data.shape[0]>1):                
-                # mav_data_final_fill = np.pad(mav_data_final,((0,len(emg_data)-len(mav_data_final)),(0,0)),'constant')  # Complete 0
-                # mav=np.r_[mav_data_final_fill.T,emg_data.T]
-                # plotter1.update_plot(mav)                                                      # mav emg&mav combined
+                plotter.update_plot(np.array(mav_data.T))
                 
               
             if keyboard.is_pressed('C'):
